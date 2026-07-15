5f0d1d7b2c0063bc63794b7e3f4e622c61fb8995
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-07-15 15:43:02 +0500

    process of sending whole playlist info done

--- routes/playlistRoute.js
+++ routes/playlistRoute.js
@@ -1,27 +1,69 @@
 const express = require("express");
+const { create } = require('youtube-dl-exec');
 const playlistUrlValidation = require("../utils/PlaylistUrlValidaton")
-const router = express.Router();
 
-router.post("/get-metadata", (req, res) => {
+const router = express.Router();
+const youtubedl = process.env.YTDLP_PATH
+    ? create(process.env.YTDLP_PATH)
+    : require("youtube-dl-exec");
+router.post("/get-playlist-data", async (req, res) => {
     try {
         const { playlistUrl } = req.body;
         if (!playlistUrl) {
             return res.json({
-                sucess: false,
+                success: false,
                 message: "please provide Valid playlistUrl"
             })
         }
         if (playlistUrlValidation(playlistUrl)) {
-            return res.send("please waite")
+            const data = await youtubedl(playlistUrl, {
+                dumpSingleJson: true,
+            });
+
+            const customObj = {
+                id: data?.id,
+                title: data?.title,
+                availability: data?.availability,
+                description: data?.description,
+                description: data?.description,
+                mainThumbnail: data?.thumbnails[0],
+                lastModified: data?.modified_date,
+                viewCount: data?.view_count,
+                playlistCount: data?.playlist_count, // number of video in one playlist
+                channelName: data?.channel,
+                channelUrl: data?.channel_url,
+                entries: data?.entries.map((entry) => { // this will be for each single video in whole pl
+                    return {
+                        thumbnail: entry?.thumbnail,
+                        description: entry?.description,
+                        duration: entry?.duration,
+                        viewCount: entry?.view_count,
+                        webpage_url: entry?.webpage_url,
+                        likeCount: entry?.like_count,
+                        availability: entry?.availability,
+                        requestedDownloads: entry?.requested_downloads[0],
+                    }
+                }),
+                _version: {
+                    version: data._version.version,
+                    repository: data._version.repository,
+                }
+            }
+            return res.json({
+                success: true,
+                message: "Playlist data Fetch Successfully..",
+                data: customObj
+            })
         } else {
             return res.json({
-                sucess: false,
+                success: false,
                 message: "please provide Valid playlistUrl"
             })
         }
     } catch (error) {
+        console.log(error)
         return res.json({
-            sucess: false,
+            success: false,
             message: error.message
         })
     }
